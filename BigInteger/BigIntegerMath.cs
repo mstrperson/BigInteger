dc61d597384cd1b7c38bd12c41e24eb3858dc1b9
Jason Cox <coxj@vt.edu>
2018-01-29 08:33:02 -0700

    more achievements.

--- BigInteger/BigIntegerMath.cs
+++ BigInteger/BigIntegerMath.cs
@@ -22,12 +22,53 @@ namespace BigInteger
 
         /// <summary>
         /// Find the Maximum value in a list
+        /// 
+        /// Easy
         /// </summary>
         /// <param name="list"></param>
         /// <returns></returns>
         public static BigInteger Max(IEnumerable<BigInteger> list)
         {
             return new BigInteger();
+        }
+
+        /// <summary>
+        /// Find the Minimum of the list.
+        /// 
+        /// Easy
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static BigInteger Min(IEnumerable<BigInteger> list)
+        {
+            return new BigInteger();
+        }
+
+        /// <summary>
+        /// Compute the Sum of the list.
+        /// 
+        /// Easy
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static BigInteger Sum(IEnumerable<BigInteger> list)
+        {
+
+            return new BigInteger();
+        }
+
+
+        /// <summary>
+        /// determine if the given BigInteger is Prime.
+        /// 
+        /// This is a Hard achievement to achieve better than O(sqrt(n)) efficiency.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static bool IsPrime(BigInteger a)
+        {
+
+            return false;
         }
     }
 }
