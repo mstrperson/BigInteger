d33dcac9e9ae20adff3c7d699065b897ffdeb9bc
NicholasDavisVMS <31394113+NicholasDavisVMS@users.noreply.github.com>
2018-02-02 10:43:57 -0700

    Update BigIntegerMath.cs

--- BigInteger/BigIntegerMath.cs
+++ BigInteger/BigIntegerMath.cs
@@ -66,6 +66,9 @@ namespace BigInteger
         /// <returns></returns>
         public static bool IsEven(BigInteger a)
         {
+            //Work for base 2,4,6,8,10,12,14,16
+            //BUT NOT WORK FOR 1,3,5,7,9,11,13,15,
+            //EX: 5F326DA: A is ten. Even vs odd bases
             if(NUMERALS.IndexOf(a.value[0]) % 2 == 1)
             {
                 return false;
